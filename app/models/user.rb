974b5e0cf58f0c1c4dbaed67f1f0bc23182aa3d3
penach0 <josemanuel.penacho@gmail.com>
2023-03-17 09:59:02 +0000

    Create validations for User attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, :email, presence: true, uniqueness: true
+  validates :name, length: { in: 4..15 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
