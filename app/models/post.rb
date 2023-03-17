5bfd7bc77a9c151c57fdecef29da2932c30e6e78
penach0 <josemanuel.penacho@gmail.com>
2023-03-17 10:08:55 +0000

    Create validations for Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, :body, presence: true
+  validates :title, uniqueness: true, length: { in: 5..50 }
+  validates :body, length: { in: 50..2000 }
 end
