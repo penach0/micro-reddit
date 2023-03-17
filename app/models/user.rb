c904ae9cb2ba6a4088e5ae29ef5d0d642e35936e
penach0 <josemanuel.penacho@gmail.com>
2023-03-17 10:18:25 +0000

    Set up association between User and Post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+
   validates :name, :email, presence: true, uniqueness: true
   validates :name, length: { in: 4..15 }
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
